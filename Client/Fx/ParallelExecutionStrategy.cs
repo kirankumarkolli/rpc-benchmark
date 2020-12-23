4a8c3631255050cda76fb98c735180d124bebe5f
kirankumarkolli <kirankk@microsoft.com>
2020-12-24 02:51:16 +0530

    Command line wait removed

--- Client/Fx/ParallelExecutionStrategy.cs
+++ Client/Fx/ParallelExecutionStrategy.cs
@@ -36,8 +36,9 @@ namespace CosmosBenchmark
                 bWarmpup: true,
                 traceFailures: traceFailures);
 
-            Console.WriteLine("WARM-UP complete. Press ENTER to continue");
-            Console.ReadLine();
+            Console.WriteLine("WARM-UP complete. Continuing for full run");
+            //Console.WriteLine("WARM-UP complete. Press ENTER to continue");
+            //Console.ReadLine();
 
             return await this.ExecuteAsyncInternal(
                 serialExecutorConcurrency,
