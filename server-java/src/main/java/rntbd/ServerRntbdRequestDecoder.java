1329cc8cc7e21d39ea10f2dc338d306b8faf0aa3
kirankumarkolli <kirankk@microsoft.com>
2020-12-26 02:25:42 +0530

    Including request decoder

--- server-java/src/main/java/rntbd/ServerRntbdRequestDecoder.java
+++ server-java/src/main/java/rntbd/ServerRntbdRequestDecoder.java
@@ -6,48 +6,73 @@ package rntbd;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.ByteToMessageDecoder;
+import io.netty.util.ReferenceCountUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 public final class ServerRntbdRequestDecoder extends ByteToMessageDecoder {
-    @Override
-    public void channelRead(final ChannelHandlerContext context, final Object message) throws Exception {
+    private final static Logger logger = LoggerFactory.getLogger(ServerRntbdRequestDecoder.class);
 
-        if (message instanceof ByteBuf) {
-
-            final ByteBuf in = (ByteBuf) message;
-            final int resourceOperationType = in.getInt(in.readerIndex() + Integer.BYTES);
-
-            if (resourceOperationType != 0) {
-                super.channelRead(context, message);
-                return;
-            }
-        }
-
-        context.fireChannelRead(message);
-    }
+//    @Override
+//    public void channelRead(final ChannelHandlerContext context, final Object message) throws Exception {
+//
+//        logger.info("Got channelRead with ctx: {} for type {} Hashcode: {}", context.name(), message.getClass().getName(), message.hashCode());
+//
+//        boolean channelReadFired = false;
+//        try {
+//
+//            if (message instanceof ByteBuf) {
+//
+//                final ByteBuf in = (ByteBuf) message;
+//
+//                final int resourceTypeInt = in.getUnsignedShort(in.readerIndex() + Integer.BYTES);
+//                final int operationTypeInt = in.getUnsignedShort(in.readerIndex() + Integer.BYTES + 2); // UINT (resource type)
+//                logger.info("channelRead resourceType: {} operatioType: {}", resourceTypeInt, operationTypeInt);
+//
+//                boolean supportType = false;
+//                switch (resourceTypeInt) {
+//                    case 0:
+//                        if (operationTypeInt == 0) {
+//                            supportType = true;
+//                        }
+//                        break;
+//                    case 2:
+//                        if (operationTypeInt == 3) {
+//                            supportType = true;
+//                        }
+//                        break;
+//                }
+//
+//                if (!supportType) {
+//                    Throwable ex = new Exception("Unsupported resourceType {" + resourceTypeInt + "} OR operationType" + operationTypeInt);
+//                    context.fireExceptionCaught(ex);
+//                }
+//            }
+//
+//            logger.info("Firing fireChannelRead for message: {} with hashcode: {}", message.getClass().getName(), message.hashCode());
+//            context.fireChannelRead(message);
+//            channelReadFired = true;
+//        }
+//        finally {
+//            if (!channelReadFired) {
+//                ReferenceCountUtil.release(message);
+//            }
+//        }
+//    }
 
     @Override
     protected void decode(
             final ChannelHandlerContext context,
             final ByteBuf in,
-            final List<Object> out) throws IllegalStateException {
+            final List<Object> out) throws Exception {
 
-        final ServerRntbdRequest request;
-        in.markReaderIndex();
+        logger.warn("[cid: 0x{} msg-id: {}] ", context.channel().id(), in.memoryAddress());
 
-        try {
-            request = ServerRntbdRequest.decode(in);
-            if(request!= null) {
-                in.discardReadBytes();
-                out.add(request);
-            } else {
-                in.resetReaderIndex();
-                return;
-            }
-        } catch (final IllegalStateException error) {
-            in.resetReaderIndex();
-            throw error;
-        }
+        final ServerRntbdRequest request = ServerRntbdRequest.decode(in);
+        logger.info("[cid: 0x{} msg-id: {}] channelRead resourceType: {} operationType: {} decoded-msg-id: {}", context.channel().id(), in.memoryAddress(), request.resourceTypeInt, request.operationTypeInt, request.hashCode());
+
+        out.add(request);
     }
 }
