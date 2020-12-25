bc5a5fdedec1930885810337f0769219d6fba061
kirankumarkolli <kirankk@microsoft.com>
2020-12-26 02:26:09 +0530

    Request framer formatting

--- server-java/src/main/java/rntbd/ServerRntbdRequestFramer.java
+++ server-java/src/main/java/rntbd/ServerRntbdRequestFramer.java
@@ -1,10 +1,19 @@
 package rntbd;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
 import java.nio.ByteOrder;
+import java.util.List;
 
 public class ServerRntbdRequestFramer extends LengthFieldBasedFrameDecoder {
     public ServerRntbdRequestFramer() {
-        super(ByteOrder.LITTLE_ENDIAN, Integer.MAX_VALUE, 0, Integer.BYTES, -Integer.BYTES, 0, true);
+        super(ByteOrder.LITTLE_ENDIAN,
+                4*1024*1024, //Integer.MAX_VALUE,
+                0,
+                Integer.BYTES,
+                -4,
+                0, // don't strip the length header
+                true);
     }
 }
