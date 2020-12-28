7539fc6e7eac5c233bc64c317aaaea47adcbd3ba
kirankumarkolli <kirankk@microsoft.com>
2020-12-28 22:08:05 +0530

    Disabling the compression for gRPC

--- GrpcService/Startup.cs
+++ GrpcService/Startup.cs
@@ -16,7 +16,10 @@ namespace GrpcService
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddGrpc();
+            services.AddGrpc(serviceOptions =>
+                {
+                    serviceOptions.ResponseCompressionLevel = System.IO.Compression.CompressionLevel.NoCompression;
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
