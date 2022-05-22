4e433d28d7888d71be1ca04f0c0009d312545f36
Kiran Kumar Kolli <kirankk@microsoft.com>
2022-05-22 09:39:35 -0700

    First draft of service

---
+++ Server/Http11Kestral/AuthorizationTokenProvider.cs
@@ -0,0 +1,25 @@
+﻿//------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//------------------------------------------------------------
+
+namespace Http11Kestral
+{
+    using System;
+    using System.Threading.Tasks;
+
+    public abstract class AuthorizationTokenProvider : IDisposable
+    {
+        public abstract string DocumentReadAuthorizationToken(
+            string resourceId,
+            string xDate,
+            IComputeHash stringHMACSHA256Helper);
+
+        public abstract string GetUserAuthorizationAsync(
+            string resourceAddress,
+            string resourceType,
+            string requestVerb,
+            string xDate);
+
+        public abstract void Dispose();
+    }
+}
---
+++ Server/Http11Kestral/Program.cs
@@ -0,0 +1,87 @@
+using Http11Kestral;
+using Microsoft.Extensions.Primitives;
+
+//------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//------------------------------------------------------------
+
+namespace Http11Kestral
+{
+    public class Startup
+    {
+        private static string emulatorKey = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<AuthorizationTokenProvider>((sp) => new AuthorizationTokenProviderMasterKey(emulatorKey));
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/dbs/{dbId}/colls/{collId}/docs/{docId}", async context =>
+                {
+                    string? dbId = (string?)context.GetRouteValue("dbId");
+                    string? collId = (string?)context.GetRouteValue("collId");
+                    string? docId = (string?)context.GetRouteValue("docId");
+                    if (dbId == null || collId == null || docId == null)
+                    {
+                        context.Response.StatusCode = 400;
+                        await context.Response.WriteAsync($"{DateTime.UtcNow.ToString()} : Incorrect addressing ({dbId}, {collId}, {docId}) ");
+                        return;
+                    }
+
+                    AuthorizationTokenProvider? authorizationTokenProvider = context.RequestServices.GetService<AuthorizationTokenProvider>();
+                    if (authorizationTokenProvider == null)
+                    {
+                        throw new Exception("AuthorizationTokenProvider is not configured");
+                    }
+
+                    // authorizationTokenProvider.DocumentReadAuthorizationToken();
+
+                    IHeaderDictionary inputHeaders = context.Request.Headers;
+                    if (inputHeaders != null)
+                    {
+                        StringValues authHeaderValue;
+                        StringValues dateHeaderValue;
+
+                        inputHeaders.TryGetValue("authorization", out authHeaderValue);
+                        inputHeaders.TryGetValue("x-ms-date", out dateHeaderValue);
+
+                        if (authHeaderValue.Count != 1
+                            || dateHeaderValue.Count != 1)
+                        {
+                            context.Response.StatusCode = 400;
+                        }
+
+                        await context.Response.WriteAsync($"{DateTime.UtcNow.ToString()} , Missing headers (auth={authHeaderValue.FirstOrDefault()}, date={dateHeaderValue.FirstOrDefault()})");
+                        return;
+                    }
+
+                    await context.Response.WriteAsync($"{DateTime.UtcNow.ToString()} GOOD TO GO!!");
+                });
+            });
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+               .ConfigureWebHostDefaults(webBuilder =>
+                    webBuilder.UseStartup<Startup>()
+                    .UseUrls("http://localhost:8080")
+                    .UseKestrel()
+                );
+        }
+    }
+}
