9387803bbde34bee7f4f72685e16766e329f6a16
Kiran Kumar Kolli <kirankk@microsoft.com>
2022-05-22 12:58:48 -0700

    Fixed bombardier script issues

--- Server/Http11Kestral/Program.cs
+++ Server/Http11Kestral/Program.cs
@@ -1,5 +1,6 @@
 using Http11Kestral;
 using Microsoft.Extensions.Primitives;
+using System.Diagnostics;
 
 //------------------------------------------------------------
 // Copyright (c) Microsoft Corporation.  All rights reserved.
@@ -32,14 +33,6 @@ namespace Http11Kestral
                         return;
                     }
 
-                    AuthorizationTokenProvider? authorizationTokenProvider = context.RequestServices.GetService<AuthorizationTokenProvider>();
-                    if (authorizationTokenProvider == null)
-                    {
-                        throw new Exception("AuthorizationTokenProvider is not configured");
-                    }
-
-                    // authorizationTokenProvider.DocumentReadAuthorizationToken();
-
                     IHeaderDictionary inputHeaders = context.Request.Headers;
                     if (inputHeaders != null)
                     {
@@ -53,11 +46,33 @@ namespace Http11Kestral
                             || dateHeaderValue.Count != 1)
                         {
                             context.Response.StatusCode = 400;
+                            await context.Response.WriteAsync($"{DateTime.UtcNow.ToString()} , Missing headers (auth={authHeaderValue.FirstOrDefault()}, date={dateHeaderValue.FirstOrDefault()})");
+                            return;
                         }
 
-                        if (context.Response.StatusCode == 400)
+                        string authTokenValue = authHeaderValue.First().Trim();
+                        string xDateValue = dateHeaderValue.First().Trim();
+
+                        if (string.IsNullOrWhiteSpace(authTokenValue) || string.IsNullOrWhiteSpace(xDateValue))
                         {
+                            context.Response.StatusCode = 400;
                             await context.Response.WriteAsync($"{DateTime.UtcNow.ToString()} , Missing headers (auth={authHeaderValue.FirstOrDefault()}, date={dateHeaderValue.FirstOrDefault()})");
+                            return;
+                        }
+
+                        AuthorizationTokenProvider? authorizationTokenProvider = context.RequestServices.GetService<AuthorizationTokenProvider>();
+                        if (authorizationTokenProvider == null)
+                        {
+                            throw new Exception("AuthorizationTokenProvider is not configured");
+                        }
+
+                        string expectedAuthValue = authorizationTokenProvider.DocumentReadAuthorizationToken(context.Request.Path, xDateValue);
+                        if (expectedAuthValue != authTokenValue)
+                        {
+                            Console.WriteLine($"xDate: {xDateValue} expected: {expectedAuthValue} actual: {authTokenValue} ");
+
+                            context.Response.StatusCode = 403;
+                            await context.Response.WriteAsync($"Auth validation failed ResourceId={context.Request.Path}, xDate={xDateValue}");
                             return;
                         }
                     }
@@ -72,6 +87,15 @@ namespace Http11Kestral
     {
         public static void Main(string[] args)
         {
+            //using (AuthorizationTokenProvider tokenProvider = AuthorizationTokenProviderMasterKey.NewEmulatorAuthProvider())
+            //{
+            //    string xDate = AuthorizationHelper.XDateHeader();
+            //    Console.WriteLine($"XDate: {xDate} ");
+            //    Console.WriteLine($"AuthHeader: {tokenProvider.DocumentReadAuthorizationToken("/dbs/db1/colls/c1/docs/d1", xDate)}");
+
+            //    Console.ReadLine();
+            //}
+
             CreateHostBuilder(args).Build().Run();
         }
 
