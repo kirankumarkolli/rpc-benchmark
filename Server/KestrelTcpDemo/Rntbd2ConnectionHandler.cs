5080aa7cb74fa072d0831e8e92f3c637f2a85b4b
Kiran Kumar Kolli <kirankk@microsoft.com>
2022-06-03 23:42:02 -0700

    Rntbd context negotiation working E2E (awesome!)

--- Server/KestrelTcpDemo/Rntbd2ConnectionHandler.cs
+++ Server/KestrelTcpDemo/Rntbd2ConnectionHandler.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Buffers;
 using System.Diagnostics;
+using System.IO;
 using System.IO.Pipelines;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Connections;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Azure.Cosmos.Rntbd;
+using Microsoft.Azure.Documents;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Options;
@@ -26,6 +29,58 @@ namespace KestrelTcpDemo
         public override async Task OnConnectedAsync(ConnectionContext connection)
         {
             var input = connection.Transport.Input;
+
+            {
+                // EntbdContext negotiation
+                ReadResult readResult = await input.ReadAtLeastAsync(4);
+                var buffer = readResult.Buffer;
+                Debug.Assert(buffer.FirstSpan.Length >= 4);
+
+                uint length = BitConverter.ToUInt32(readResult.Buffer.FirstSpan.Slice(0, sizeof(UInt32)));
+
+                while (buffer.Length < length) // Read at-least length
+                {
+                    input.AdvanceTo(buffer.Start, readResult.Buffer.End);
+                }
+
+                // TODO: Incoming context validation 
+                // 16 <- Activity id (hard coded)
+                int connectionContextOffet = sizeof(UInt32) + sizeof(UInt16) + sizeof(UInt16) + 16;
+
+                BytesDeserializer reader = new BytesDeserializer(buffer.Slice(connectionContextOffet, length-connectionContextOffet).ToArray(), (int) length - connectionContextOffet);
+                RntbdConstants.ConnectionContextRequest request = new RntbdConstants.ConnectionContextRequest();
+                request.ParseFrom(ref reader);
+
+
+                // Send response 
+
+                //RntbdConstants.ConnectionContextHeadersResponse responseHeaders = new RntbdConstants.ConnectionContextHeadersResponse();
+                //responseHeaders.activityId.value.valueGuid = Guid.NewGuid();
+                //responseHeaders.activityId.isPresent = true;
+
+                //responseHeaders.statusCode.value.valueULong = 200;
+                //responseHeaders.statusCode.isPresent = true;
+
+
+                // TODO: Fill right values 
+                RntbdConstants.ConnectionContextResponse contextResponse = new RntbdConstants.ConnectionContextResponse();
+
+                contextResponse.protocolVersion.value.valueULong = RntbdConstants.CurrentProtocolVersion;
+                contextResponse.protocolVersion.isPresent = true;
+
+                contextResponse.serverVersion.value.valueBytes = HttpConstants.Versions.CurrentVersionUTF8;
+                contextResponse.serverVersion.isPresent = true;
+
+                contextResponse.clientVersion.value.valueBytes = HttpConstants.Versions.CurrentVersionUTF8;
+                contextResponse.clientVersion.isPresent = true;
+
+                contextResponse.serverAgent.value.valueBytes = Encoding.UTF8.GetBytes("RntbdServer");
+                contextResponse.serverAgent.isPresent = true;
+
+                byte[] responseMessage = contextResponse.Serialize(200, Guid.NewGuid());
+                await connection.Transport.Output.WriteAsync(new Memory<byte>(responseMessage));
+            }
+
 
             // Code to parse length prefixed encoding
             while (true)
