12080d041e98f9855967e6f657e53bc47682a564
Kiran Kumar Kolli <kirankk@microsoft.com>
2022-06-03 21:53:04 -0700

    Some more refactoring

---
+++ Server/KestrelTcpDemo/Rntbd2ConnectionHandler.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Buffers;
+using System.Diagnostics;
+using System.IO.Pipelines;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Connections;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
+
+namespace KestrelTcpDemo
+{
+    // This is the connection handler the framework uses to handle new incoming connections
+    internal class Rntbd2ConnectionHandler : ConnectionHandler
+    {
+        private readonly IRntbdMessageParser _parser;
+
+        public Rntbd2ConnectionHandler(IRntbdMessageParser parser)
+        {
+            _parser = parser;
+        }
+
+        public override async Task OnConnectedAsync(ConnectionContext connection)
+        {
+            var input = connection.Transport.Input;
+
+            // Code to parse length prefixed encoding
+            while (true)
+            {
+                ReadResult readResult = await input.ReadAtLeastAsync(4);
+                var buffer = readResult.Buffer;
+                Debug.Assert(buffer.FirstSpan.Length >= 4);
+
+                uint length = BitConverter.ToUInt32(readResult.Buffer.FirstSpan.Slice(0, sizeof(UInt32)));
+
+                while (buffer.Length < length) // Read at-least message
+                {
+                    input.AdvanceTo(buffer.Start, readResult.Buffer.End);
+                }
+
+
+                if (_parser.TryParseMessage(ref buffer, out var message))
+                {
+                    await ProcessMessageAsync(connection, buffer, message);
+                }
+
+                input.AdvanceTo(buffer.Start, buffer.End);
+            }
+        }
+
+        private async Task ProcessMessageAsync(
+            ConnectionContext connection,
+            ReadOnlySequence<byte> buffer,
+            RntbdMessage message)
+        {
+            foreach (var segment in buffer)
+            {
+                await connection.Transport.Output.WriteAsync(segment);
+            }
+        }
+    }
+}
---
+++ Server/KestrelTcpDemo/RntbdExtensions.cs
@@ -0,0 +1,88 @@
+﻿// This shows how a framework would implement a custom connection handler 
+
+using System;
+using System.Buffers;
+using System.Diagnostics;
+using System.IO.Pipelines;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Connections;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
+
+namespace KestrelTcpDemo
+{
+    public static class RntbdExtensions
+    {
+        public static IServiceCollection AddFramework(this IServiceCollection services, IPEndPoint endPoint)
+        {
+            services.TryAddEnumerable(ServiceDescriptor.Singleton<IConfigureOptions<KestrelServerOptions>, Rntbd2OptionsSetup>());
+
+            services.Configure<Rntbd2OptionsSetup.Rntbd2Options>(o =>
+            {
+                o.EndPoint = endPoint;
+            });
+
+            services.TryAddSingleton<IRntbdMessageParser, RntbdMessageParser>();
+            return services;
+        }
+    }
+
+    public class Rntbd2OptionsSetup : IConfigureOptions<KestrelServerOptions>
+    {
+        private readonly Rntbd2Options _options;
+
+        public Rntbd2OptionsSetup(IOptions<Rntbd2Options> options)
+        {
+            _options = options.Value;
+        }
+
+        public void Configure(KestrelServerOptions options)
+        {
+            options.Listen(_options.EndPoint, builder =>
+            {
+                builder.UseConnectionHandler<Rntbd2ConnectionHandler>();
+            });
+        }
+
+        // The framework exposes options for how to bind
+        public class Rntbd2Options
+        {
+            public IPEndPoint EndPoint { get; set; }
+        }
+    }
+
+
+    // The framework exposes a message parser used to parse incoming protocol messages from the network
+    public interface IRntbdMessageParser
+    {
+        bool TryParseMessage(ref ReadOnlySequence<byte> buffer, out RntbdMessage message);
+    }
+
+    public class RntbdMessageParser : IRntbdMessageParser
+    {
+        public bool TryParseMessage(ref ReadOnlySequence<byte> buffer, out RntbdMessage message)
+        {
+            message = null;
+            string content = Encoding.ASCII.GetString(buffer.ToArray(), 0, (int)buffer.Length);
+
+            // Check for end-of-file tag. If it is not there, read
+            // more data.  
+            if (content.IndexOf("<EOF>") > -1)
+            {
+                message = new RntbdMessage();
+                return true;
+            }
+
+            return false;
+        }
+    }
+
+    public class RntbdMessage
+    {
+        // TODO: Add properties relevant to your message type here
+    }
+}
