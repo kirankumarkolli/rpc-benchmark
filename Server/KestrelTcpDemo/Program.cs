14951f744ab1c3d898d9c0736dd0e6d7c8fa60af
Kiran Kumar Kolli <kirankk@microsoft.com>
2022-06-06 00:25:30 -0700

    starting http1, http2, Rntbd2 server OnStartup
    logging providers cleared

--- Server/KestrelTcpDemo/Program.cs
+++ Server/KestrelTcpDemo/Program.cs
@@ -1,7 +1,9 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Connections;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace KestrelTcpDemo
 {
@@ -9,33 +11,47 @@ namespace KestrelTcpDemo
     {
         public static void Main(string[] args)
         {
+            // AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
             CreateWebHostBuilder(args).Build().Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .ConfigureServices(services =>
-                {
-                    // This shows how a custom framework could plug in an experience without using Kestrel APIs directly
-                    services.AddFramework(new IPEndPoint(IPAddress.Loopback, 8009));
-                })
+                    {
+                        // This shows how a custom framework could plug in an experience without using Kestrel APIs directly
+                        services.AddFramework(new IPEndPoint(IPAddress.Loopback, 8009));
+                    })
                 .UseKestrel(options =>
-                {
-                    //// TCP 8007
-                    //options.ListenLocalhost(8007, builder =>
-                    //{
-                    //    builder.UseConnectionHandler<MyEchoConnectionHandler>();
-                    //});
+                    {
+                        //// TCP 8007
+                        //options.ListenLocalhost(8007, builder =>
+                        //{
+                        //    builder.UseConnectionHandler<MyEchoConnectionHandler>();
+                        //});
 
-                    //// HTTP 5000
-                    //options.ListenLocalhost(5000);
+                        //// HTTP 5000
+                        //options.ListenLocalhost(5000);
 
-                    //// HTTPS 5001
-                    //options.ListenLocalhost(5001, builder =>
-                    //{
-                    //    builder.UseHttps();
-                    //});
-                })
+                        //// HTTPS 5001
+                        //options.ListenLocalhost(5001, builder =>
+                        //{
+                        //    builder.UseHttps();
+                        //});
+
+                        options.ListenLocalhost(7070,
+                            listenOptions => listenOptions.Protocols = Microsoft.AspNetCore.Server.Kestrel.Core.HttpProtocols.Http1
+                            );
+
+                        options.ListenLocalhost(8080, 
+                            listenOptions => listenOptions.Protocols = Microsoft.AspNetCore.Server.Kestrel.Core.HttpProtocols.Http2
+                            );
+                    })
+                .ConfigureLogging((context, loggingBuilder) => 
+                    {
+                        loggingBuilder.ClearProviders();
+                    })
                 .UseStartup<Startup>();
     }
 }
