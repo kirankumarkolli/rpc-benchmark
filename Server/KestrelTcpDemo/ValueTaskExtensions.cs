1ac1943a9fec457c9bcceaff0716d90c757cfd16
Kiran Kumar Kolli <kirankk@microsoft.com>
2022-06-11 20:42:23 -0700

    Kestral Tsl1.2 clone code separation

---
+++ Server/KestrelTcpDemo/ValueTaskExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System.IO.Pipelines;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace Kestrel.Clone
+{
+    internal static class ValueTaskExtensions
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Task GetAsTask(this in ValueTask<FlushResult> valueTask)
+        {
+            // Try to avoid the allocation from AsTask
+            if (valueTask.IsCompletedSuccessfully)
+            {
+                // Signal consumption to the IValueTaskSource
+                valueTask.GetAwaiter().GetResult();
+                return Task.CompletedTask;
+            }
+            else
+            {
+                return valueTask.AsTask();
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ValueTask GetAsValueTask(this in ValueTask<FlushResult> valueTask)
+        {
+            // Try to avoid the allocation from AsTask
+            if (valueTask.IsCompletedSuccessfully)
+            {
+                // Signal consumption to the IValueTaskSource
+                valueTask.GetAwaiter().GetResult();
+                return default;
+            }
+            else
+            {
+                return new ValueTask(valueTask.AsTask());
+            }
+        }
+    }
+}
