728c472643c8b61f8e23498f44d271d1f11cf5dc
Matias Quaranta <ealsur@hotmail.com>
2022-06-27 15:38:37 -0700

    Wiring pipes

--- FE/Program.cs
+++ FE/Program.cs
@@ -1,6 +1,20 @@
 // 1 input on HTTP port 8000 and maintain 4 connections for TCP routing per partition (assuming requests to the same partition)
+// Backend/TCP servers should be started with ports 8009/8010/8011/8012
 
+using FE;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Documents;
+using System.IO.Pipelines;
+
+string authKey = "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
+int maxConnectionsPerServer;
+if (!int.TryParse(Environment.GetCommandLineArgs()[1], out maxConnectionsPerServer){
+    maxConnectionsPerServer = 30;
+}
+
+TransportClient tcpTransportClient = RequestHelper.CreateTcpClient(maxConnectionsPerServer);
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,17 +27,59 @@ builder.WebHost.ConfigureKestrel((context, options) =>
     });
 });
 
+builder.Services.AddSingleton<IComputeHash>((sp) => new StringHMACSHA256Hash(authKey));
+
 var app = builder.Build();
 
 app.Run(async context =>
 {
-    // Obtain the hash of the request to know which partition to route to
-    
     // Obtain the type of request
+    if (!RequestHelper.TryClassifyRequest(context.Request, out OperationType operationType)
+        || operationType == OperationType.None)
+    {
+        context.Response.StatusCode = 400;
+        await context.Response.WriteAsync($"{DateTime.UtcNow} : Cannot classify request.");
+        return;
+    }
 
-    // Route to one of the established connections and pipe the response
-    var req = context.Request;
-    await context.Response.WriteAsync("Hello from 2nd delegate.");
+    if (operationType == OperationType.Write)
+    {
+        context.Response.StatusCode = 400;
+        await context.Response.WriteAsync($"{DateTime.UtcNow} : Not supporting Write requests atm.");
+        return;
+    }
+
+    if (!RequestHelper.TryRouteToEndpoint(context.Request, out (Uri, DocumentServiceRequest) routingInformation))
+    {
+        context.Response.StatusCode = 400;
+        await context.Response.WriteAsync($"{DateTime.UtcNow} : Could not build request routing information");
+        return;
+    }
+
+    using (ActivityScope activityScope = new ActivityScope(Guid.NewGuid()))
+    {
+        Microsoft.Azure.Documents.StoreResponse storeResponse = await tcpTransportClient.InvokeStoreAsync(
+            physicalAddress: routingInformation.Item1,
+            resourceOperation: Microsoft.Azure.Documents.ResourceOperation.ReadDocument,
+            request: routingInformation.Item2);
+
+        if (storeResponse.StatusCode != System.Net.HttpStatusCode.OK || storeResponse.ResponseBody == null)
+        {
+            throw new Exception($"Unexpected status code {storeResponse.StatusCode}");
+        }
+
+        // drain response
+        using (storeResponse.ResponseBody)
+        {
+            context.Response.StatusCode = (int)storeResponse.StatusCode;
+            context.Response.Headers.ContentLength = storeResponse.ResponseBody.Length;
+            context.Response.Headers.Add(HttpConstants.HttpHeaders.RequestCharge, "1.0");
+
+            PipeWriter pipeWriter = context.Response.BodyWriter;
+            PipeReader pipeReader = PipeReader.Create(storeResponse.ResponseBody);
+            await pipeReader.CopyToAsync(pipeWriter);
+        }
+    }
 });
 
 app.Run();
@@ -32,9 +88,28 @@ app.Run();
 // Write goes to primary, Read to any
 enum OperationType
 {
-    Create,
+    None,
+    Write,
     Read
 }
 
+readonly ref struct RequestRoutingInformation
+{
+    public RequestRoutingInformation(
+        ReadOnlySpan<char> database,
+        ReadOnlySpan<char> container,
+        ReadOnlySpan<char> documentId,
+        ReadOnlySpan<char> partitionKeyValue)
+    {
+        this.Database = database;
+        this.Container = container;
+        this.DocumentId = documentId;
+        this.PartitionKeyValue = partitionKeyValue;
+    }
 
+    public ReadOnlySpan<char> Database { get; init; }
+    public ReadOnlySpan<char> Container { get; init; }
+    public ReadOnlySpan<char> DocumentId { get; init; }
 
+    public ReadOnlySpan<char> PartitionKeyValue { get; init; }
+}
---
+++ FE/RequestHelper.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.Azure.Documents;
+using Microsoft.Extensions.Primitives;
+
+namespace FE
+{
+    internal static class RequestHelper
+    {
+        public static bool TryClassifyRequest(HttpRequest request, out OperationType operationType)
+        {
+            operationType = OperationType.None;
+            
+            if (request.Method == HttpMethod.Get.Method)
+            {
+                operationType = OperationType.Read;
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool TryGetRoutingInformation(HttpRequest request, out RequestRoutingInformation requestRoutingInformation)
+        {
+            requestRoutingInformation = default;
+
+            if (!request.Path.HasValue)
+            {
+                return false;
+            }
+
+            ReadOnlySpan<char> path = request.Path.Value.AsSpan();
+
+            int currentIndex = 0;
+            int nextIndex = 0;
+            ReadOnlySpan<char> value = ReadOnlySpan<char>.Empty;
+            ReadOnlySpan<char> database = ReadOnlySpan<char>.Empty;
+            ReadOnlySpan<char> container = ReadOnlySpan<char>.Empty;
+            ReadOnlySpan<char> documentId = ReadOnlySpan<char>.Empty;
+            ReadOnlySpan<char> partitionKeyValue = ReadOnlySpan<char>.Empty;
+            // dbs/{db}/colls/{container}/docs/{id}
+
+            while ((nextIndex = path.Slice(currentIndex).IndexOf('/')) > -1)
+            {
+                value = path.Slice(currentIndex, nextIndex - currentIndex);
+                if (database.IsEmpty)
+                {
+                    if (value != "dbs".AsSpan())
+                    {
+                        database = value;
+                    }
+                }
+                else if (container.IsEmpty)
+                {
+                    if (value != "colls".AsSpan())
+                    {
+                        container = value;
+                    }
+                }
+
+                currentIndex = ++nextIndex;
+            }
+
+            value = path.Slice(currentIndex, nextIndex - currentIndex);
+
+            if (database.IsEmpty)
+            {
+                if (value != "dbs".AsSpan())
+                {
+                    database = value;
+                }
+            }
+            else if (container.IsEmpty)
+            {
+                if (value != "colls".AsSpan())
+                {
+                    container = value;
+                }
+            }
+            else if (documentId.IsEmpty)
+            {
+                if (value != "docs".AsSpan())
+                {
+                    documentId = value;
+                }
+            }
+
+            if (request.Headers.TryGetValue(Microsoft.Azure.Documents.HttpConstants.HttpHeaders.PartitionKey, out StringValues partitionKey))
+            {
+                partitionKeyValue = partitionKey[0].AsSpan();
+            }
+
+            requestRoutingInformation = new RequestRoutingInformation(
+                    database,
+                    container,
+                    documentId,
+                    partitionKeyValue
+                );
+
+            return false;
+        }
+
+        public static TransportClient CreateTcpClient(int maxConnectionsPerServer)
+        {
+            return new Microsoft.Azure.Documents.Rntbd.TransportClient(
+                new Microsoft.Azure.Documents.Rntbd.TransportClient.Options(TimeSpan.FromSeconds(240))
+                {
+                    MaxChannels = maxConnectionsPerServer,
+                    PartitionCount = 8,
+                    MaxRequestsPerChannel = 10,
+                    //PortReuseMode = PortReuseMode.,
+                    //PortPoolReuseThreshold = rntbdPortPoolReuseThreshold,
+                    //PortPoolBindAttempts = rntbdPortPoolBindAttempts,
+                    ReceiveHangDetectionTime = TimeSpan.FromSeconds(20),
+                    SendHangDetectionTime = TimeSpan.FromSeconds(20),
+                    //UserAgent = "TestClient",
+                    //CertificateHostNameOverride = overrideHostNameInCertificate,
+                    OpenTimeout = TimeSpan.FromSeconds(240),
+                    TimerPoolResolution = TimeSpan.FromSeconds(2),
+                    IdleTimeout = TimeSpan.FromSeconds(20),
+                    EnableCpuMonitor = true,
+                    // CallerId = callerId,
+                    //ConnectionStateListener = this.connectionStateListener
+                });
+        }
+
+        public static bool TryRouteToEndpoint(HttpRequest request, out (Uri, DocumentServiceRequest) routing)
+        {
+            routing = default;
+            RequestRoutingInformation requestRoutingInformation;
+            if (!RequestHelper.TryGetRoutingInformation(request, out requestRoutingInformation))
+            {
+                return false;
+            }
+        }
+    }
+}
