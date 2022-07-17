71f7b6249a694155bbc20816f414df3154d6f518
Kiran Kumar Kolli <kirankk@microsoft.com>
2022-07-16 19:03:56 -0700

    Some more refractoring

---
+++ Common/CosmosDuplexPipe .Connection.cs
@@ -0,0 +1,93 @@
+﻿//------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//------------------------------------------------------------
+
+namespace CosmosBenchmark
+{
+    using System;
+    using System.Diagnostics;
+    using System.Net.Sockets;
+    using System.Threading;
+    using Microsoft.Azure.Cosmos.Core.Trace;
+
+    internal partial class CosmosDuplexPipe : IDisposable
+    {
+        internal static void SetCommonSocketOptions(Socket clientSocket)
+        {
+            clientSocket.SetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.NoDelay, true);
+            EnableTcpKeepAlive(clientSocket);
+        }
+
+        private static void EnableTcpKeepAlive(Socket clientSocket)
+        {
+            clientSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.KeepAlive, true);
+
+#if !NETSTANDARD15 && !NETSTANDARD16
+            // This code should use RuntimeInformation.IsOSPlatform(OSPlatform.Windows),
+            // but the feature is unavailable on .NET Framework 4.5.1.
+            if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+            {
+                try
+                {
+                    clientSocket.IOControl(
+                        IOControlCode.KeepAliveValues,
+                        keepAliveConfiguration.Value,
+                        null);
+                }
+                catch (Exception e)
+                {
+                    DefaultTrace.TraceWarning("IOControl(KeepAliveValues) failed: {0}", e);
+                    // Ignore the exception.
+                }
+            }
+#endif  // !NETSTANDARD15 && !NETSTANDARD16
+        }
+
+        private static readonly Lazy<byte[]> keepAliveConfiguration =
+            new Lazy<byte[]>(GetWindowsKeepAliveConfiguration,
+                LazyThreadSafetyMode.ExecutionAndPublication);
+
+        private static byte[] GetWindowsKeepAliveConfiguration()
+        {
+            const uint EnableKeepAlive = 1;
+            const uint KeepAliveIntervalMs = 30 * 1000;
+            const uint KeepAliveRetryIntervalMs = 1 * 1000;
+
+            //  struct tcp_keepalive
+            //  {
+            //      u_long  onoff;
+            //      u_long  keepalivetime;
+            //      u_long  keepaliveinterval;
+            //  };
+            byte[] keepAliveConfig = new byte[3 * sizeof(uint)];
+            BitConverter.GetBytes(EnableKeepAlive).CopyTo(keepAliveConfig, 0);
+            BitConverter.GetBytes(KeepAliveIntervalMs).CopyTo(keepAliveConfig, sizeof(uint));
+            BitConverter.GetBytes(KeepAliveRetryIntervalMs).CopyTo(keepAliveConfig, 2 * sizeof(uint));
+            return keepAliveConfig;
+        }
+
+        private static void SetReuseUnicastPort(Socket clientSocket)
+        {
+#if !NETSTANDARD15 && !NETSTANDARD16
+            // This code should use RuntimeInformation.IsOSPlatform(OSPlatform.Windows),
+            // but the feature is unavailable on .NET Framework 4.5.1.
+            if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+            {
+                try
+                {
+                    Debug.Assert(!clientSocket.IsBound);
+                    // SocketOptionName.ReuseUnicastPort is only present in .NET Framework 4.6.1 and newer.
+                    // Use the numeric value for as long as this code needs to target earlier versions.
+                    const int SO_REUSE_UNICASTPORT = 0x3007;
+                    clientSocket.SetSocketOption(SocketOptionLevel.Socket, (SocketOptionName)SO_REUSE_UNICASTPORT, true);
+                }
+                catch (Exception e)
+                {
+                    DefaultTrace.TraceWarning("SetSocketOption(Socket, ReuseUnicastPort) failed: {0}", e);
+                    // Ignore the exception.
+                }
+            }
+#endif  // !NETSTANDARD15 && !NETSTANDARD16
+        }
+    }
+}
--- Common/CosmosDuplexPipe.cs
+++ Common/CosmosDuplexPipe.cs
@@ -27,7 +27,7 @@ using Microsoft.Azure.Cosmos;
     using System.Collections.Concurrent;
 using Microsoft.Azure.Cosmos.Core.Trace;
 
-    internal class CosmosDuplexPipe : IDisposable
+    internal partial class CosmosDuplexPipe : IDisposable
     {
         private static readonly string AuthorizationFormatPrefixUrlEncoded = HttpUtility.UrlEncode(string.Format(CultureInfo.InvariantCulture, Constants.Properties.AuthorizationFormat,
                 Constants.Properties.MasterToken,
@@ -207,7 +207,7 @@ using Microsoft.Azure.Cosmos.Core.Trace;
             }
         }
 
-        public static async Task<CosmosDuplexPipe> Connect(Uri endpoint)
+        public static async Task<CosmosDuplexPipe> ConnectAsClient(Uri endpoint)
         {
             IPAddress resolvedAddress = await ResolveHostAsync(endpoint.DnsSafeHost);
             TcpClient tcpClient = new TcpClient(resolvedAddress.AddressFamily);
@@ -244,7 +244,7 @@ using Microsoft.Azure.Cosmos.Core.Trace;
                     new LengthPrefixPipeReader(pipeReader), 
                     pipeWriter);
 
-            await CosmosDuplexPipe.NegotiateRntbdContext(duplexPipe);
+            await duplexPipe.NegotiateRntbdContextAsClient();
             return duplexPipe;
         }
 
@@ -258,11 +258,11 @@ using Microsoft.Azure.Cosmos.Core.Trace;
             responseType.ParseFrom(ref bytesDeserializer);
         }
 
-        private static async Task NegotiateRntbdContext(CosmosDuplexPipe duplexPipe)
+        public async Task NegotiateRntbdContextAsClient()
         {
-            await duplexPipe.SendRntbdContext();
+            await this.SendRntbdContext();
 
-            (UInt32 length, byte[] messageBytes) = await duplexPipe.Reader.MoveNextAsync(isLengthCountedIn: true);
+            (UInt32 length, byte[] messageBytes) = await this.Reader.MoveNextAsync(isLengthCountedIn: true);
 
             // TODO: Incoming context validation 
             // sizeof(UInt32) -> Length-prefix
@@ -314,126 +314,6 @@ using Microsoft.Azure.Cosmos.Core.Trace;
             this.pipeWriter.Advance(length);
 
             return this.pipeWriter.FlushAsync();
-        }
-
-
-        internal class RntbdRequestPool
-        {
-            public static readonly RntbdRequestPool Instance = new RntbdRequestPool();
-
-            private readonly ConcurrentQueue<RntbdConstants.Request> requests = new ConcurrentQueue<RntbdConstants.Request>();
-
-            private RntbdRequestPool()
-            {
-            }
-
-            public RequestOwner Get()
-            {
-                if (this.requests.TryDequeue(out RntbdConstants.Request request))
-                {
-                    return new RequestOwner(request);
-                }
-
-                return new RequestOwner(new RntbdConstants.Request());
-            }
-
-            private void Return(RntbdConstants.Request request)
-            {
-                request.Reset();
-                this.requests.Enqueue(request);
-            }
-
-            public readonly struct RequestOwner : IDisposable
-            {
-                public RequestOwner(RntbdConstants.Request request)
-                {
-                    this.Request = request;
-                }
-
-                public RntbdConstants.Request Request { get; }
-
-                public void Dispose()
-                {
-                    RntbdRequestPool.Instance.Return(this.Request);
-                }
-            }
-        }
-        internal static void SetCommonSocketOptions(Socket clientSocket)
-        {
-            clientSocket.SetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.NoDelay, true);
-            EnableTcpKeepAlive(clientSocket);
-        }
-
-        private static void EnableTcpKeepAlive(Socket clientSocket)
-        {
-            clientSocket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.KeepAlive, true);
-
-#if !NETSTANDARD15 && !NETSTANDARD16
-            // This code should use RuntimeInformation.IsOSPlatform(OSPlatform.Windows),
-            // but the feature is unavailable on .NET Framework 4.5.1.
-            if (Environment.OSVersion.Platform == PlatformID.Win32NT)
-            {
-                try
-                {
-                    clientSocket.IOControl(
-                        IOControlCode.KeepAliveValues,
-                        keepAliveConfiguration.Value,
-                        null);
-                }
-                catch (Exception e)
-                {
-                    DefaultTrace.TraceWarning("IOControl(KeepAliveValues) failed: {0}", e);
-                    // Ignore the exception.
-                }
-            }
-#endif  // !NETSTANDARD15 && !NETSTANDARD16
-        }
-
-        private static readonly Lazy<byte[]> keepAliveConfiguration =
-            new Lazy<byte[]>(GetWindowsKeepAliveConfiguration,
-                LazyThreadSafetyMode.ExecutionAndPublication);
-
-        private static byte[] GetWindowsKeepAliveConfiguration()
-        {
-            const uint EnableKeepAlive = 1;
-            const uint KeepAliveIntervalMs = 30 * 1000;
-            const uint KeepAliveRetryIntervalMs = 1 * 1000;
-
-            //  struct tcp_keepalive
-            //  {
-            //      u_long  onoff;
-            //      u_long  keepalivetime;
-            //      u_long  keepaliveinterval;
-            //  };
-            byte[] keepAliveConfig = new byte[3 * sizeof(uint)];
-            BitConverter.GetBytes(EnableKeepAlive).CopyTo(keepAliveConfig, 0);
-            BitConverter.GetBytes(KeepAliveIntervalMs).CopyTo(keepAliveConfig, sizeof(uint));
-            BitConverter.GetBytes(KeepAliveRetryIntervalMs).CopyTo(keepAliveConfig, 2 * sizeof(uint));
-            return keepAliveConfig;
-        }
-
-        private static void SetReuseUnicastPort(Socket clientSocket)
-        {
-#if !NETSTANDARD15 && !NETSTANDARD16
-            // This code should use RuntimeInformation.IsOSPlatform(OSPlatform.Windows),
-            // but the feature is unavailable on .NET Framework 4.5.1.
-            if (Environment.OSVersion.Platform == PlatformID.Win32NT)
-            {
-                try
-                {
-                    Debug.Assert(!clientSocket.IsBound);
-                    // SocketOptionName.ReuseUnicastPort is only present in .NET Framework 4.6.1 and newer.
-                    // Use the numeric value for as long as this code needs to target earlier versions.
-                    const int SO_REUSE_UNICASTPORT = 0x3007;
-                    clientSocket.SetSocketOption(SocketOptionLevel.Socket, (SocketOptionName)SO_REUSE_UNICASTPORT, true);
-                }
-                catch (Exception e)
-                {
-                    DefaultTrace.TraceWarning("SetSocketOption(Socket, ReuseUnicastPort) failed: {0}", e);
-                    // Ignore the exception.
-                }
-            }
-#endif  // !NETSTANDARD15 && !NETSTANDARD16
         }
     }
 }
